9af2a05f561329d98592edb1a2bff7edb9f0e925
Joao Anastacio <almeida.jar97@gmail.com>
2023-11-24 20:25:00 -0300

    added machines page

--- app.py
+++ app.py
@@ -7,16 +7,45 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def student():
-   return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/machine/<string:iname>',methods=['GET'])
+@app.route('/machines/<string:iname>',methods=['GET'])
 def machine(iname):
     global board
     query_result=get_machine_data(iname)
     return render_template('machine.html',sname=iname,obj=query_result)
-    pass
 
+
+@app.route('/machines')
+def machine_base():
+    timeline_data = generate_timeline_data()
+    return render_template('machine.html',timeline_data=timeline_data)
+
+
+
+
+
+
+
+
+
+def generate_timeline_data():
+    timeline_data = []
+
+    for i in range(8,18):
+        hour_label = f'{i}:00'
+        timeline_data.append({'label': hour_label, 'bars': generate_bars()})
+    return timeline_data
+
+def generate_bars():
+    bars=[]
+    for j in range(12):
+        start=j * 5
+        status='idle' if j>5 else 'patient'
+        label='joao da silva'if status=='patient' else ''
+        bars.append({'start': start, 'status':status, 'label':label})
+    return bars
 
 
 if __name__ == '__main__':
